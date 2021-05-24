7cf2d3105106e4707508f57764e5ad0d3e7fdd2e
maruyu31 <you3746336@gmail.com>
2021-05-24 15:06:20 +0000

    [Add] createとupdateの成功時にフラッシュメッセージの表示とvalidatesによる失敗時の表示を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,21 +13,28 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(book.id), notice: "Book was successfully update."
+    else
+      render :edit
+    end
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(book.id), notice: "Book was successfully create."
+    else
+      render :index
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path, notice: "Book was successfully destroyed."
   end
 
   private
